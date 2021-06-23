05231564f505c590bfbbfa48957b254c6d5afff8
Michael Ashby <mhoa@ucla.edu>
2021-06-23 09:12:42 -0700

    updated models.py and image_test.py

--- image_test.py
+++ image_test.py
@@ -11,6 +11,7 @@ import tensorflow_datasets as tfds
 import pickle
 from flax import serialization
 import models
+import random
 
 
 def load_optimizer(opt_pkl):
@@ -25,8 +26,10 @@ def load_image():
     ds_builder.download_and_prepare()
     test_ds = tfds.as_numpy(ds_builder.as_dataset(split='test', batch_size=-1, shuffle_files = True))
     test_ds['image'] = jnp.float32(test_ds['image']) / 255.
-    image = test_ds['image'][0]
-    return image #(28,28,1)
+    image = random.choice(test_ds['image'])
+    image = image.reshape(1,28,28,1)
+    print(f'Image dimensions = {image.shape}')
+    return image #(1,28,28,1)
 
 def autoencoder_loss(logits, image):
    # if type(logits) or type(image) == NoneType:
